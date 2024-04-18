919c2803b17c55422e0ab408811a0fb766b88427
saekar08 <167098286+saekar08@users.noreply.github.com>
2024-04-18 19:44:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ TUT_VID = os.environ.get("TUT_VID","https://t.me/one_am_stories_tutorial/9")
 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002130033900"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "1002130033900"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
